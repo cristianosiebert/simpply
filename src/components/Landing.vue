e97a1f95c1742bfe03b59440f15d4d35f32ec535
Cristiano Siebert <cristiano.siebert@trust.group>
2024-09-15 13:30:32 -0300

    feat: ajuste erro metodo

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -190,6 +190,9 @@ onMounted(() => {
             <p>
               E se houvesse uma solução que resolvesse esses problemas, mas também transformasse a maneira como sua equipe aprende e executa tarefas?
               Imagine um treinamento que não só melhora a retenção e o engajamento, mas também reduz significativamente os erros operacionais.
+            </p>
+            <br />
+            <p>
               Com o Simpply, você pode transformar esses desafios em oportunidades e alcançar resultados que antes pareciam inatingíveis.
               Não deixe que a falta de inovação continue a custar caro à sua empresa.
               Descubra como uma abordagem prática e interativa pode fazer toda a diferença!
